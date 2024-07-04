95f6aa76e7374000f91f1c7c363feaf25e2ebdd4
Shyamak Seth <sethshyamak@gmail.com>
2024-07-04 17:47:41 +0530

    added city 3d

--- routes/cityRouter.js
+++ routes/cityRouter.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const {ensurePass} = require('../utils/auth.js')
 
-router.get('/', ensurePass, (req, res) => {
+// router.get('/', ensurePass, (req, res) => {
+//     res.render('city')
+// });
+router.get('/', (req, res) => {
     res.render('city')
 });
 
